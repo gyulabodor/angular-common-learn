a19c70a3c2a87e5a2021e4c1940b1eb0f49d9947
bodorgyula <Gyula.Bodor@kh.hu>
2024-01-21 02:01:26 +0100

    create recipe-form component

--- reactive_imperative/src/app/recipe/recipe.module.ts
+++ reactive_imperative/src/app/recipe/recipe.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RecipeRoutingModule} from "./recipe-routing.module";
 import { RecipePageComponent } from "./recipe-page/recipe-page.component";
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
 
 
 
 @NgModule({
-  declarations: [RecipePageComponent],
+  declarations: [RecipePageComponent, RecipeFormComponent],
   imports: [
     RecipeRoutingModule,
     CommonModule
