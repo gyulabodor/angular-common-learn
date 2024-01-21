5ec55d241b5d38d0aaaf26373424d00692f8847e
bodorgyula <Gyula.Bodor@kh.hu>
2024-01-21 01:57:45 +0100

    create recipe feature module and routing with lazy-loading

---
+++ reactive_imperative/src/app/recipe/recipe.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RecipeRoutingModule} from "./recipe-routing.module";
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RecipeRoutingModule,
+    CommonModule
+  ]
+})
+export class RecipeModule { }
